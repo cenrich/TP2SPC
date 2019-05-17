bbe9f8b15843de21300a296d0e4fdaf2d48d67fb
pifernandez <pilfernandez1@gmail.com>
2019-05-17 17:38:52 -0300

    funcion que agrega tarea a mejorar

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 // funcion que toma lo que se ingresa en el input, lo guarda y lo imprime en pantalla por debajo como tarea pendiente
-var item, task, toDo
+
+var item, task, toDo, container
 var addTask=function(){
-    // busca en el dom el id del input asi queda guardardo en una variable
-    //el value del input (la tarea que ingresa la persona) la guarda en una variable
     item=document.getElementById('item')
     task=item.value
-    // crea un elemento li y le ingresa el texto de la tarea que se ingreso
-    var container=document.createElement('li')
+
+    container=document.createElement('li')
     container.innerText=task
-    //busca en el dom el ul que tiene como id 'toDo' y le agrega un hijo: el li que creamos antes
+
     toDo=document.getElementById('toDo')
     toDo.appendChild(container)
-    // creo un div para contener los dos botones, estos tambien los creo. a los tres elementos les doy una clase asi se le pueden agregar los estilos. despues los agrego como hijos del li
+
     var containerButtons=document.createElement('div')
     containerButtons.classList.add('button')
     container.appendChild(containerButtons)
@@ -23,9 +22,18 @@ var addTask=function(){
     var buttonCheck=document.createElement('button')
     buttonCheck.classList.add('check')
     containerButtons.appendChild(buttonCheck)
-    //crea el evento onclick
+
+    var containerTask=document.getElementsByClassName('toDo')
+    var completed=document.getElementById("completed")
+
     buttonCheck.onclick=function(){
-        //tiene que hacer que la tarea ya no figure en el ul toDo sino que en el completed
+        completed.appendChild(container)
     }
-    
+
+    buttonRemove.onclick=function(){
+        var completed=document.getElementById("completed")
+        toDo.removeChild(container)
+    }
+
 }
+// hay que mejorar la funcion para que quede mas chica, como vimos en clase. dividirla en pequeñas funciones
